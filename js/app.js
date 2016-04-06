eb3c48840f2c6affeecca9d76ce7b02a7406975e
EngineerEmily <Emily00@gmail.com>
2016-04-06 02:07:33 -0700

    starting frame for positions page.

--- js/app.js
+++ js/app.js
@@ -1,1 +1,17 @@
 $(document).foundation();
+
+$('.toggle').click(function(e) {
+  	e.preventDefault();
+  
+    var $this = $(this);
+  
+    if ($this.next().hasClass('show')) {
+        $this.next().removeClass('show');
+        $this.next().slideUp(350);
+    } else {
+        $this.parent().parent().find('li .inner').removeClass('show');
+        $this.parent().parent().find('li .inner').slideUp(350);
+        $this.next().toggleClass('show');
+        $this.next().slideToggle(350);
+    }
+});
