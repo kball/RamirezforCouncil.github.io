e895b13dba66c7343a278d822732a1290e110229
Kevin Ball <kmball11@gmail.com>
2016-06-15 21:13:13 -0700

    Add autoexpand/contract with scroll

--- js/app.js
+++ js/app.js
@@ -1,5 +1,18 @@
 $(document).foundation();
 
+$(document).ready(function() {
+  var lastLink;
+  $('#nav-magellan').on('update.zf.magellan', function(event, link) {
+    // slightly expensive, so only fire once per change in link
+    if(link[0] !== lastLink) {
+      lastLink = link[0];
+      var menu = $(link).parents('.submenu');
+      if(!(menu.is(':visible'))) {
+        $(menu).siblings('a').click();
+      }
+    }
+  });
+});
 $('.toggle').click(function(e) {
   	e.preventDefault();
   
