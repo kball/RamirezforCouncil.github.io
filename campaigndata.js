9cad6b0c33a9add91accd4e2c16cb1b28184ff06
EngineerEmily <Emily00@gmail.com>
2017-07-27 14:07:56 -0700

    Added new GeoJSON

---
+++ campaigndata.js
@@ -0,0 +1,789 @@
+{
+  "type": "FeatureCollection",
+  "features": [
+    {
+      "type": "Feature",
+      "properties": {
+        "City": "New York",
+        "State": "NY ",
+        "Zip": "10024",
+        "Amount": "25"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -73.97418739999999,
+          40.7859464
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "City": "Smyrna",
+        "State": "TN ",
+        "Zip": "37167",
+        "Amount": "50"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -86.5299673,
+          35.96321330000001
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "City": "Malibu",
+        "State": "CA ",
+        "Zip": "90265",
+        "Amount": 50
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -118.8363756,
+          34.0615492
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "City": "Cathedral City",
+        "State": "CA ",
+        "Zip": "92234",
+        "Amount": 200
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -116.4547962,
+          33.82632359999999
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "City": "Belmont",
+        "State": "CA ",
+        "Zip": "94002",
+        "Amount": 400
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -122.2900288,
+          37.5178022
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "City": "Burlingame",
+        "State": "CA ",
+        "Zip": "94010",
+        "Amount": 450
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -122.367428,
+          37.5685247
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "City": "Half Moon Bay",
+        "State": "CA ",
+        "Zip": "94019",
+        "Amount": 50
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -122.4353995,
+          37.4903082
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "City": "Los Altos",
+        "State": "CA ",
+        "Zip": "94022",
+        "Amount": 1403
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -122.1405413,
+          37.3694009
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "City": "Los Altos",
+        "State": "CA ",
+        "Zip": "94024",
+        "Amount": 1350
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -122.1007867,
+          37.3478257
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "City": "Menlo Park",
+        "State": "CA ",
+        "Zip": "94025",
+        "Amount": 574
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -122.1802812,
+          37.4484914
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "City": "Millbrae",
+        "State": "CA ",
+        "Zip": "94030",
+        "Amount": 99
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -122.4184108,
+          37.5958949
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "City": "Mountain View",
+        "State": "CA ",
+        "Zip": "94039",
+        "Amount": 295
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -122.08,
+          37.39
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "City": "Mountain View",
+        "State": "CA ",
+        "Zip": "94040",
+        "Amount": 4202
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -122.086585,
+          37.3785351
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "City": "Mountain View",
+        "State": "CA ",
+        "Zip": "94041",
+        "Amount": 4197
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -122.0751549,
+          37.386812
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "City": "Mountain View",
+        "State": "CA ",
+        "Zip": "94042",
+        "Amount": 131.53
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -122.0778761,
+          37.3931794
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "City": "Mountain View",
+        "State": "CA ",
+        "Zip": "94043",
+        "Amount": 2478
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -122.0723816,
+          37.428434
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "City": "Redwood City",
+        "State": "CA ",
+        "Zip": "94061",
+        "Amount": 125
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -122.2256803,
+          37.4634447
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "City": "Redwood City",
+        "State": "CA ",
+        "Zip": "94062",
+        "Amount": "100"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -122.2880726,
+          37.41089270000001
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "City": "San Carlos",
+        "State": "CA ",
+        "Zip": "94070",
+        "Amount": 25
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -122.2710602,
+          37.4975165
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "City": "Sunnyvale",
+        "State": "CA ",
+        "Zip": "94086",
+        "Amount": 675
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -122.0212337,
+          37.371859
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "City": "Sunnyvale",
+        "State": "CA ",
+        "Zip": "94087",
+        "Amount": 1075
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -122.0326019,
+          37.3492097
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "City": "Sunnyvale",
+        "State": "CA ",
+        "Zip": "94089",
+        "Amount": 550
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -122.0181762,
+          37.4110966
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "City": "Palo Alto",
+        "State": "CA ",
+        "Zip": "94301",
+        "Amount": 1048
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -122.1575745,
+          37.4457966
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "City": "Palo Alto",
+        "State": "CA ",
+        "Zip": "94303",
+        "Amount": 715
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -122.1178261,
+          37.4530553
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "City": "Stanford",
+        "State": "CA ",
+        "Zip": "94305",
+        "Amount": "100"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -122.1689284,
+          37.4135757
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "City": "Palo Alto",
+        "State": "CA ",
+        "Zip": "94306",
+        "Amount": 2235
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -122.1235054,
+          37.4177563
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "City": "San Mateo",
+        "State": "CA ",
+        "Zip": "94401",
+        "Amount": "100"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -122.3164207,
+          37.5793536
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "City": "San Mateo",
+        "State": "CA ",
+        "Zip": "94402",
+        "Amount": 600
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -122.3390936,
+          37.5245965
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "City": "San Mateo",
+        "State": "CA ",
+        "Zip": "94404",
+        "Amount": 60
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -122.2710602,
+          37.5549479
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "City": "Fremont",
+        "State": "CA ",
+        "Zip": "94538",
+        "Amount": 250
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -121.9643745,
+          37.5042267
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "City": "Hayward",
+        "State": "CA ",
+        "Zip": "94545",
+        "Amount": "100"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -122.1178261,
+          37.6063621
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "City": "Oakland",
+        "State": "CA ",
+        "Zip": "94605",
+        "Amount": 99
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -122.1462193,
+          37.7554905
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "City": "Campbell",
+        "State": "CA ",
+        "Zip": "95008",
+        "Amount": 125
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -121.9529992,
+          37.2770029
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "City": "Cupertino",
+        "State": "CA ",
+        "Zip": "95014",
+        "Amount": 30
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -122.0723816,
+          37.31317
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "City": "Santa Cruz",
+        "State": "CA ",
+        "Zip": "95062",
+        "Amount": "100"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -121.9871217,
+          36.9677899
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "City": "San Jose",
+        "State": "CA ",
+        "Zip": "95110",
+        "Amount": 300
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -121.918866,
+          37.354611
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "City": "San Jose",
+        "State": "CA ",
+        "Zip": "95112",
+        "Amount": 500
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -121.8847222,
+          37.3456227
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "City": "San Jose",
+        "State": "CA ",
+        "Zip": "95113",
+        "Amount": 200
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -121.8918364,
+          37.3326639
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "City": "San Jose",
+        "State": "CA ",
+        "Zip": "95117",
+        "Amount": 25
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -121.9643745,
+          37.3120731
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "City": "San Jose",
+        "State": "CA ",
+        "Zip": "95121",
+        "Amount": 50
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -121.8050125,
+          37.302108
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "City": "San Jose",
+        "State": "CA ",
+        "Zip": "95122",
+        "Amount": 87
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -121.8391808,
+          37.3304014
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "City": "San Jose",
+        "State": "CA ",
+        "Zip": "95124",
+        "Amount": 150
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -121.918866,
+          37.2583724
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "City": "San Jose",
+        "State": "CA ",
+        "Zip": "95125",
+        "Amount": 225
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -121.8904136,
+          37.2909813
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "City": "San Jose",
+        "State": "CA ",
+        "Zip": "95128",
+        "Amount": 125
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -121.9416226,
+          37.3189149
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "City": "San Jose",
+        "State": "CA ",
+        "Zip": "95130",
+        "Amount": 50
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -121.9857002,
+          37.2876164
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "City": "San Jose",
+        "State": "CA ",
+        "Zip": "95132",
+        "Amount": 175
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -121.8643855,
+          37.4010609
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "City": "San Jose",
+        "State": "CA ",
+        "Zip": "95135",
+        "Amount": "100"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -121.685341,
+          37.2751505
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "City": "San Jose",
+        "State": "CA ",
+        "Zip": "95138",
+        "Amount": 10
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -121.7309452,
+          37.2422863
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "City": "San Jose",
+        "State": "CA ",
+        "Zip": "95148",
+        "Amount": 99
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -121.7708337,
+          37.3315827
+        ]
+      }
+    }
+  ]
+}
